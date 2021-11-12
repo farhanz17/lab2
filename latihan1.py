e96627637321d6a66cfba88d391174945650aa3f
farhanz17 <mufhan623@gmail.com>
2021-11-12 22:10:57 +0700

    farhan menambahkan latihan1

--- latihan1.py
+++ latihan1.py
@@ -3,7 +3,7 @@
 print("="*20)
 
 mobil_ijul = input("ijul memiliki mobil sebanyak : ")
-mobil_saha = input("ijul memiliki mobil sebanyak : ")
+mobil_saha = input("saha memiliki mobil sebanyak : ")
 
 if mobil_ijul > mobil_saha:
     print("Benar, lebih banyak")
