27d775cf4d591a64ea8d12c9279e6995a4817c53
farhanz17 <mufhan623@gmail.com>
2021-11-14 16:35:41 +0700

    Tentbilterbesar.py ditambahkan

---
+++ Tentbilterbesar.py
@@ -0,0 +1,15 @@
+#Menentukan bilangan terbesar menggunakan statement if
+
+print("~"*39)
+
+kelereng_udin = input("Udin memiliki kelereng sebanyak : ")
+kelereng_budi = input("Budi memiliki kelereng sebanyak : ")
+
+if kelereng_udin == kelereng_udin:
+    if kelereng_udin > kelereng_budi:
+        print("Benar, kelereng Udin lebih banyak ")
+    else :
+        print("Salah, kelereng Budi lebih banyak")
+
+
+            
